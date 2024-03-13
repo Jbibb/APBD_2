575646b1f1eb0a05d9425dc2a28e6052433bbee8
Jbibb <julbar25@gmail.com>
2024-03-13 17:23:01 +0100

    Zaimplementowano metode liczaca srednia

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,3 +4,12 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("#1 Hello, World!");
 Console.WriteLine("#2 Hello, World!");
 Console.WriteLine("#3 Hello, World!");
+
+static double getAvg(int[] array)
+{
+    double res = 0;
+    for (int i = 0; i < array.Length; i++)
+        res += array[i];
+    res /= array.Length;
+    return res;
+}
